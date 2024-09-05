10030a091c8b65cf51bb654d36057d75df72f556
FaiqRindha <faiq.rindha11@gmail.com>
2024-09-05 20:51:53 +0700

    'portfolio'

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        'primary': '#ECDFCC',
+        'secondary': '#697565',
+        'dark': '#1E201E',
+        'green': '#9CA986',
+        'darkgreen': '#1E201E',
+        'textpanjang': '#64748b',
+        'client': '#3C3D37',
+        'footer': '#1E201E'
+      },
+      screens: {
+        '2xl': '1320px',
+      }
+    },
+    plugins: [],
+  }
+}
+
